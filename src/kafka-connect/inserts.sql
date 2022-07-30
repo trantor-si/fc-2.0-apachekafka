140f879a5b9707a54f6261723f15059943ba2c23
trantor-si <luccostajr@gmail.com>
2022-07-30 12:37:30 -0300

    feat: :sparkles: create final producer / consumer source and mysql / mongodb kafkaconnect

---
+++ src/kafka-connect/inserts.sql
@@ -0,0 +1,17 @@
+insert into categories (name) values ('Eletronicos');
+insert into categories (name) values ('Perfuraria');
+insert into categories (name) values ('Papelaria');
+insert into categories (name) values ('Livros');
+insert into categories (name) values ('Moveis');
+insert into categories (name) values ('Eletrodomesticos');
+insert into categories (name) values ('Jardinagem');
+insert into categories (name) values ('Cama, Mesa e Banho');
+insert into categories (name) values ('Decoracao');
+insert into categories (name) values ('Outros');
+insert into categories (name) values ('Roupas');
+insert into categories (name) values ('Calcados');
+insert into categories (name) values ('Acessorios');
+insert into categories (name) values ('Bazar');
+insert into categories (name) values ('Jogos');
+insert into categories (name) values ('Esportes');
+
